e03d2ce751d83de9c9fe942ac4492f5316267715
vikingrag <100153634+vikingrag@users.noreply.github.com>
2022-10-23 18:52:03 +0200

    Donwload file REDME

---
+++ app/src/main/java/com/example/miwetherapp223/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.miwetherapp223;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final android.R.attr R = ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
